2f0df7b111f4044962e3d36e0dc02f4c82489c4e
Biancavitoriav <124395956+Biancavitoriav@users.noreply.github.com>
2025-03-02 15:07:52 -0300

    Adding header and response

--- meu-rpa-site/src/pages/home.tsx
+++ meu-rpa-site/src/pages/home.tsx
@@ -1,15 +1,17 @@
-
-import { Grid, TextField, Button, Checkbox, FormControlLabel, Typography } from "@mui/material";
-
+import { Grid, TextField, Button, Checkbox, FormControlLabel, Typography, Box, Stack } from "@mui/material";
+import emailIcon from "../assets/email-icon.png";
 export default function EmailSummaryForm() {
   return (
     <Grid container columns={12} spacing={2} padding={3} >
-      
-      {/* Cabeçalho */}
       <Grid item xs={12}>
-        <Typography >
-          HEADER
-        </Typography>
+      <Box
+      sx={{
+        background: "linear-gradient(to right, #C1BEED, #EC9EB4)",
+        padding: 2
+      }}>
+        <Typography fontWeight="bold" marginLeft="60px"><img src={emailIcon} alt="icon" width={24} height={24} style={{ marginRight: 8, marginTop: 8 }}/>
+        Resumindo seus e-mails</Typography>
+    </Box>
       </Grid>
 
       {/* Formulário - Lado Esquerdo */}
@@ -22,12 +24,6 @@ export default function EmailSummaryForm() {
         <TextField fullWidth label="Data" margin="normal" />
         <TextField fullWidth label="Email" margin="normal" />
         <TextField fullWidth label="Senha" margin="normal" type="password" />
-
-        <Typography sx={{ marginTop: 1 }}>Selecione qual plataforma você utiliza:</Typography>
-        <FormControlLabel control={<Checkbox />} label="Gmail" />
-        <br/>
-        <FormControlLabel control={<Checkbox />} label="Outlook" />
-        <br/>
         <br/>
         <Button fullWidth variant="contained" sx={{ marginTop: 2, fontWeight: 800  }}>
           RESUMIR OS E-MAILS
@@ -36,9 +32,36 @@ export default function EmailSummaryForm() {
 
       {/* Resumo - Lado Direito */}
       <Grid item xs={12} md={8}>
-        <Typography>RESPOSTA</Typography>
+        <Box marginTop="50px" marginLeft="30px" border="1px solid rgb(38, 38, 38)" borderRadius="5px" padding="20px"
+        >
+      <Typography width="px">
+        Resposta:
+      </Typography>
+ 
+      <Stack padding="2">
+        <Box>
+          <Typography fontWeight="bold" marginTop="10px">Leitura de e-mails</Typography>
+          <Typography variant="body2" color="text.secondary" marginTop="10px">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </Typography>
+        </Box>
+        <Box>
+          <Typography fontWeight="bold" marginTop="10px">Leitura de e-mails</Typography>
+          <Typography variant="body2" color="text.secondary" marginTop="10px">
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and 
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="body2" fontSize="1" marginTop="10px" fontWeight="bold">
+            Leitura de e-mails
+          </Typography>
+          <Typography variant="body2" color="text.secondary" marginTop="10px">
+          centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </Typography>
+        </Box>
+      </Stack>
+    </Box>
       </Grid>
-
     </Grid>
   );
 }
